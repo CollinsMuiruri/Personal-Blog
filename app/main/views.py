dbdba6b9f58d89346b2aab16467b5d9417ebdf57
Collins Muiruri <wanyekicollins@gmail.com>
2018-04-23 20:17:36 +0300

    Added routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,21 @@
+from flask import render_template
 from . import main
 
 @main.route('/')
 def index():
-    return '<h1> Hello World </h1>'
 
+    return render_template('index.html')
+@main.route('/about')
+def about():
+
+    return render_template("about.html")
+
+@main.route('/signup')
+def signup():
+
+    return render_template("signup.html")
+
+@main.route('/login')
+def login():
+
+    return render_template("login.html")
